46e20ac034ca0542290b54aeed6014795405e69e
Parsa <p_sadri_s@yahoo.com>
2020-11-03 23:42:15 +0330

    add suport for wordcloud

--- extract_time_data.py
+++ extract_time_data.py
@@ -1,7 +1,68 @@
 from telethon import TelegramClient, events, sync
 from datetime import timezone, datetime, timedelta 
 import numpy as np
+import matplotlib.pyplot as plt
+import re
+from PIL import Image
+from persian_wordcloud.wordcloud import PersianWordCloud, add_stop_words
+
 from draw import draw
+
+
+
+def create_wordcloud(username, messages):
+    def removeWeirdChars(text):
+        weridPatterns = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u'\U00010000-\U0010ffff'
+                                   u"\u200d"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\u3030"
+                                   u"\ufe0f"
+                                   u"\u2069"
+                                   u"\u2066"
+                                   u"\u200c"
+                                   u"\u2068"
+                                   u"\u2067"
+                                   "]+", flags=re.UNICODE)
+        return weridPatterns.sub(r' ', text)
+
+    def clean_tweets(raw_list):
+        while None in raw_list:
+            raw_list.remove(None)
+        raw_string = ' '.join(raw_list)
+        no_links = re.sub(r'http\S+', '', raw_string)
+        no_links = re.sub(r'\S+.com', '', no_links)
+        return no_links
+
+    def create_words(clean_string):
+        words = clean_string.split(" ")
+        words = [w for w in words if len(w) > 3]  # ignore a, to, at...
+        return words
+
+    raw_tweets = messages
+    clean_text = clean_tweets(raw_tweets)
+    words = create_words(clean_text)
+    clean_string = ','.join(words)
+    mask = np.array(Image.open('twitter-logo.jpg'))
+    wc = PersianWordCloud(background_color="white", max_words=2000, mask=mask)
+    clean_string = removeWeirdChars(clean_string)
+    wc.generate(clean_string)
+    plt.clf()
+    plt.cla()
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.savefig(username + '.png', bbox_inches='tight')
 
 
 
@@ -33,10 +94,15 @@ num_total_receive = 0
 count_conversation = 0
 last_msg_date = 0
 last_msg_id = 0
+messages_recieve = []
+messages_sent = []
 i = 0
 for message in client.iter_messages(username):
-    # if i == 10:
-    #     break
+    # print(message.text)
+    if message.sender.id == my_id:
+        messages_sent.append(message.text)
+    else:
+        messages_recieve.append(message.text)
     message_time = message.date.replace(tzinfo=timezone.utc).astimezone(tz=None)
     message_times.append([message_time, message.sender.id == my_id])
     i += 1
@@ -83,3 +149,5 @@ with open(str(username)+"_analysis.txt", "w") as text_file:
 file_path = str(username) + "_times.npy"
 np.save(file_path, np.array(message_times))
 draw(file_path, True)
+create_wordcloud(username, messages_recieve)
+create_wordcloud("me_"+username, messages_sent)
